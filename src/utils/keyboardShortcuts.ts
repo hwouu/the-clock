cfae1e605a42c862970c42d52b6e0f85df2a98bc
Hyunwoo <nhw3990@kau.kr>
2025-04-20 15:38:57 +0900

    feat: 키보드 단축키 및 유틸리티 기능 추가
    - 키보드 단축키 지원 (시계 모드, 테마, 타이머, 메모)
    - 첫 방문 시 단축키 안내 표시 기능
    - 유틸리티 함수 모듈화

---
+++ src/utils/keyboardShortcuts.ts
@@ -0,0 +1,50 @@
+// src/utils/keyboardShortcuts.ts
+import { useEffect } from "react";
+import { useTimerStore } from "../store/timerStore";
+import { useMemoStore } from "../store/memoStore";
+
+interface KeyboardShortcutsProps {
+  toggleClockMode: () => void;
+  toggleTheme: () => void;
+}
+
+export const useKeyboardShortcuts = ({
+  toggleClockMode,
+  toggleTheme,
+}: KeyboardShortcutsProps) => {
+  const { openModal: openTimerModal } = useTimerStore();
+  const { openModal: openMemoModal } = useMemoStore();
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      // Cmd/Ctrl + 키 조합 확인
+      const isModifier = event.metaKey || event.ctrlKey;
+
+      if (isModifier) {
+        switch (event.key) {
+          case "d": // 다크/라이트 모드 전환
+            event.preventDefault();
+            toggleTheme();
+            break;
+          case "m": // 시계 모드 전환 (아날로그/디지털)
+            event.preventDefault();
+            toggleClockMode();
+            break;
+          case "t": // 타이머 열기
+            event.preventDefault();
+            openTimerModal();
+            break;
+          case "n": // 새 메모 열기
+            event.preventDefault();
+            openMemoModal();
+            break;
+          default:
+            break;
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [toggleClockMode, toggleTheme, openTimerModal, openMemoModal]);
+};
